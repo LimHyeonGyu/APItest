1fd27050a0c8b424fa76d094709f63f8d7159381
LimHyeunGyu <delldro94@gmail.com>
2024-11-10 20:26:03 +0900

    first commit

---
+++ src/main/kotlin/edu/example/api_test/ApiTestApplication.kt
@@ -0,0 +1,11 @@
+package edu.example.api_test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApiTestApplication>(*args)
+}
